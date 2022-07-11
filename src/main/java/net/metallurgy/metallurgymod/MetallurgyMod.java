a73513f733c6af09e15e5d85fcb573690cbe8b4f
Funcrafter4 <funcrafter123456@gmail.com>
2022-07-12 01:58:34 +0600

    Initial commit

---
+++ src/main/java/net/metallurgy/metallurgymod/MetallurgyMod.java
@@ -0,0 +1,16 @@
+package net.metallurgy.metallurgymod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MetallurgyMod implements ModInitializer {
+
+	public static final String MOD_ID = "metallurgymod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/metallurgy/metallurgymod/MetallurgyModClient.java
@@ -0,0 +1,10 @@
+package net.metallurgy.metallurgymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MetallurgyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
