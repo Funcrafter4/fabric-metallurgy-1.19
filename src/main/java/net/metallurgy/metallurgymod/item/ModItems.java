e82340089a9243c74a0eac83fe6ed42739a7eea5
Funcrafter4 <funcrafter123456@gmail.com>
2022-07-12 13:21:34 +0600

    Raw Silver Ore + Silver Ingot + Smelting

--- src/main/java/net/metallurgy/metallurgymod/item/ModItems.java
+++ src/main/java/net/metallurgy/metallurgymod/item/ModItems.java
@@ -3,7 +3,6 @@ package net.metallurgy.metallurgymod.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.metallurgy.metallurgymod.MetallurgyMod;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -16,6 +15,10 @@ public class ModItems {
     public static final Item  MANGANESE_DUST = registerItem("manganese_dust", new Item(new FabricItemSettings().group(ModItemGroup.METALLURGY_GROUP)));
 
     public static final Item  MANGANESE_INGOT = registerItem("manganese_ingot", new Item(new FabricItemSettings().group(ModItemGroup.METALLURGY_GROUP)));
+
+    public static final Item RAW_SILVER_ORE = registerItem("raw_silver_ore", new Item(new FabricItemSettings().group(ModItemGroup.METALLURGY_GROUP)));
+
+    public static final Item  SILVER_INGOT = registerItem("silver_ingot", new Item(new FabricItemSettings().group(ModItemGroup.METALLURGY_GROUP)));
 
 
     private static Item registerItem(String name, Item item){
