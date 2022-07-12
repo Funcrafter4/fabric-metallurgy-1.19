944edb3800cabe7c3ad688e7bde94197e038948c
Funcrafter4 <funcrafter123456@gmail.com>
2022-07-12 12:54:10 +0600

    Manganese Dust and Ingot + Smelting

--- src/main/java/net/metallurgy/metallurgymod/item/ModItems.java
+++ src/main/java/net/metallurgy/metallurgymod/item/ModItems.java
@@ -9,9 +9,14 @@ import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
-    public static final Item RAW_TIN_ORE = registerItem("raw_tin_ore", new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
+    public static final Item RAW_TIN_ORE = registerItem("raw_tin_ore", new Item(new FabricItemSettings().group(ModItemGroup.METALLURGY_GROUP)));
 
-    public static final Item TIN_INGOT = registerItem("tin_ingot", new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
+    public static final Item TIN_INGOT = registerItem("tin_ingot", new Item(new FabricItemSettings().group(ModItemGroup.METALLURGY_GROUP)));
+
+    public static final Item  MANGANESE_DUST = registerItem("manganese_dust", new Item(new FabricItemSettings().group(ModItemGroup.METALLURGY_GROUP)));
+
+    public static final Item  MANGANESE_INGOT = registerItem("manganese_ingot", new Item(new FabricItemSettings().group(ModItemGroup.METALLURGY_GROUP)));
+
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registry.ITEM, new Identifier(MetallurgyMod.MOD_ID, name), item);
