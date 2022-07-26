51fb29e5bc36529bd6609722f43b836c788bf6a9
ObscurumOne <zhenya.21022000@gmail.com>
2022-07-26 09:30:04 +0300

    Add blocks

--- src/main/java/net/metallurgy/metallurgymod/block/ModBlocks.java
+++ src/main/java/net/metallurgy/metallurgymod/block/ModBlocks.java
@@ -44,6 +44,42 @@ public class ModBlocks {
     public static final Block COMPRESSOR_BLOCK = registerBlock("compressor_block",
             new CompressorBlock(FabricBlockSettings.of(Material.METAL)), ModItemGroup.METALLURGY_GROUP);
 
+    public static final Block ZINC_BLOCK = registerBlock("zinc_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.METALLURGY_GROUP);
+
+    public static final Block TIN_BLOCK = registerBlock("tin_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.METALLURGY_GROUP);
+
+    public static final Block SILVER_BLOCK = registerBlock("silver_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.METALLURGY_GROUP);
+
+    public static final Block PLATINUM_BLOCK = registerBlock("platinum_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.METALLURGY_GROUP);
+
+    public static final Block MANGANESE_BLOCK = registerBlock("manganese_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.METALLURGY_GROUP);
+
+    public static final Block ANGMALLEN_BLOCK = registerBlock("angmallen_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.METALLURGY_GROUP);
+
+    public static final Block BRASS_BLOCK = registerBlock("brass_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.METALLURGY_GROUP);
+
+    public static final Block BRONZE_BLOCK = registerBlock("bronze_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.METALLURGY_GROUP);
+
+    public static final Block HEPATIZON_BLOCK = registerBlock("hepatizon_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.METALLURGY_GROUP);
+
+    public static final Block STEEL_BLOCK = registerBlock("steel_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.METALLURGY_GROUP);
+
+    public static final Block DAMASCUS_STEEL_BLOCK = registerBlock("damascus_steel_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.METALLURGY_GROUP);
+
+    public static final Block ELECTRUM_BLOCK = registerBlock("electrum_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.METALLURGY_GROUP);
+
     private static Block registerBlock(String name, Block block, ItemGroup group){
         registerBlockItem(name, block, group);
         return Registry.register(Registry.BLOCK,new Identifier(MetallurgyMod.MOD_ID, name),block);
